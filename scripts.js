d68649767221e246d483b85567443b48672e437f
Pricey-93 <56130851+Pricey-93@users.noreply.github.com>
2020-08-15 23:27:57 +0100

    design improvements and button javascript added

--- scripts.js
+++ scripts.js
@@ -1,11 +1,27 @@
 "use strict"
+/* Reading action listeners*/
 document.getElementById("hiraganaButton").addEventListener("click", function(){toggleDisplay("hiraganaReading"); })
-document.getElementById("englishButton").addEventListener("click", function(){toggleDisplay("englishTranslation"); })
+document.getElementById("hiraganaButton").addEventListener("click", function(){toggleShowHide("hiraganaButton"); })
 
+/* Translation action listeners*/
+document.getElementById("englishButton").addEventListener("click", function(){toggleDisplay("englishTranslation"); })
+document.getElementById("englishButton").addEventListener("click", function(){toggleShowHide("englishButton"); })
+
+
+/* toggles the reading / translation visibility */
 function toggleDisplay(id) {
     if (document.getElementById(id).style.display !== "block"){
     document.getElementById(id).style.display = "block";
     }
     else
     document.getElementById(id).style.display = "none";
+ }
+
+ /* toggles the button prompt between show/hide */
+ function toggleShowHide(id) {
+     if (document.getElementById(id).innerHTML === "show") {
+         document.getElementById(id).innerHTML = "hide";
+     }
+     else
+     document.getElementById(id).innerHTML = "show";
  }
