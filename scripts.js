d84acf2f7d5a7dee7cb94b96d47386daf9b44484
Pricey-93 <56130851+Pricey-93@users.noreply.github.com>
2020-08-18 00:13:09 +0100

    Generator function complete + design tweaks.

--- scripts.js
+++ scripts.js
@@ -7,6 +7,9 @@ document.getElementById("hiraganaButton").addEventListener("click", function(){t
 document.getElementById("englishButton").addEventListener("click", function(){toggleDisplay("englishTranslation"); })
 document.getElementById("englishButton").addEventListener("click", function(){toggleShowHide("englishButton"); })
 
+/* Generator action listeners*/
+document.getElementById("kanjiButton").addEventListener("click", function(){loadTextFile("kanji-sets/all_year_one_kanji.txt"); })
+document.getElementById("kanjiButton").addEventListener("click", refresh);
 
 /* toggles the reading / translation visibility */
 function toggleDisplay(id) {
@@ -24,4 +27,40 @@ function toggleDisplay(id) {
      }
      else
      document.getElementById(id).innerHTML = "show";
+ }
+
+ /* toggles display and changes button promps when generating new kanji */
+ function refresh() {
+     document.getElementById("hiraganaReading").style.display = "none";
+     document.getElementById("englishTranslation").style.display = "none";
+     document.getElementById("hiraganaButton").innerHTML = "show";
+     document.getElementById("englishButton").innerHTML = "show";
+ }
+
+/* loads kanji file and generates random line*/
+ function loadTextFile(filename) {
+     fetch(filename)
+     .then(function(response) {
+         return response.text() ;
+    })
+    .then(function(data) {
+        const ALL_LINES = data.split(/\r\n|\n/);
+        let math = Math.floor(Math.random() * Math.floor(ALL_LINES.length -1));
+        let line = ALL_LINES[math];
+        let split = line.split(" ");
+        let kanji_line = split[0];
+        let reading_line = split[1];
+        let english_line = [];
+        let i = 2;
+        while (i !== split.length) {
+            english_line += split[i] + " ";
+            i++;
+        }   
+        document.getElementById("kanjiCharacter").innerHTML = kanji_line;
+        document.getElementById("hiraganaReading").innerHTML = reading_line;
+        document.getElementById("englishTranslation").innerHTML = english_line;
+    })
+    .catch(function(error) {
+        console.log(error);
+    })
  }
